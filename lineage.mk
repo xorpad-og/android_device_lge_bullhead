b4fb68676a11c60419d152ca66eacf80aee3131d
xorpad <xorpad@nixphone.info>
2017-11-05 19:43:24 -0500

    first commit

---
+++ lineage.mk
@@ -0,0 +1,21 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := lineage_bullhead
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 5X
+TARGET_MANUFACTURER := LGE
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=bullhead \
+    BUILD_FINGERPRINT="google/bullhead/bullhead:7.1.2/N2G47W/3938523:user/release-keys" \
+    PRIVATE_BUILD_DESC="bullhead-user 7.1.2 N2G47W 3938523 release-keys"
